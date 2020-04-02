f11692843f4ad98cfe8c339690b91f87e0d2449f
Vancer <supremevance@gmail.com>
2020-04-02 14:37:52 -0500

    del

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Head from "next/head";
-import "../public/css/tailwind.css"
-
-function MyApp({Component, pageProps}) {
-    return <Component {...pageProps} />
-}
-
-export default MyApp;
