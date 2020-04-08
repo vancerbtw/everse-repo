82d660b326fb769d93bc185dfb0757f935600f41
Vancer <supremevance@gmail.com>
2020-04-08 09:57:36 -0500

    next.js bs

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  distDir: "_next",
+  generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  }
+}
