298474fa250df6e66f98f6ba4dde33d01874ff8c
Vancer <supremevance@gmail.com>
2020-04-08 10:06:47 -0500

    stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
-import "../css/sileo.css";
-import React from "react";
+import '../styles/index.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
